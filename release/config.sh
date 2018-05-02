a9ad67506b11cd0e7e3440069ff689ab5d858b4d
cloudfreexiao <996442717qqcom@gmail.com>
2018-05-02 15:24:05 +0800

    change release config

--- release/config.sh
+++ release/config.sh
@@ -2,11 +2,7 @@
 
 # ip    username   save path
 SERVER_LIST=(\
-    10.10.1.112         dev     /home/dev/              内网测试 \
-    203.195.185.157     root    /data/hsgame/           外网测试 \
-    115.159.93.130      hgame   /home/hgame/            外网审核 \
-    123.206.200.131     hsgame  /data/hsgame/           商务包服 \
-    122.152.212.251     hsgame  /data/hsgame/           uc测试服 \
+    127.0.0.1         dev     /opt/RillServer              内网测试 \
     )
 
 GAME_NAME="server"
